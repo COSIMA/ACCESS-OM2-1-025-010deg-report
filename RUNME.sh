3dc8664b68b90b1400da28b31c676ae5823d7322
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2017-10-06 18:29:16 +1100

    more gitinfo fixes

--- RUNME.sh
+++ RUNME.sh
@@ -4,6 +4,9 @@
 cp gitinfo-git-hook.txt .git/hooks/post-checkout
 cp gitinfo-git-hook.txt .git/hooks/post-commit
 cp gitinfo-git-hook.txt .git/hooks/post-merge
+chmod +x .git/hooks/post-checkout
+chmod +x .git/hooks/post-commit
+chmod +x .git/hooks/post-merge
 # and make .git/gitHeadInfo.git for gitinfo package
 git checkout
 exit 0
