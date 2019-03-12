5ddfb22a1561d61aa1f6fefeb659c8ba034837ea
andy <Andy.Hogg@anu.edu.au>
2019-03-13 00:53:44 +1100

    Fixing equatorial pacific. Part-way through edits to WOCE transects. Trying to run zonal temp salt bias

--- figures/WOCE_transects/Plot_WOCE_transects_one_touch.py
+++ figures/WOCE_transects/Plot_WOCE_transects_one_touch.py
@@ -20,8 +20,6 @@ from distributed.diagnostics.progressbar import progress
 import cosima_cookbook
 KELVIN_TO_CELSIUS = -273.15
 
-### NOTE - I AM PART-WAY THROUGH ADAPTING THIS TO MAKE 8-PANEL FIOHGURES!! -- AH
-
 PLOT_BIAS = True
 
 
@@ -39,19 +37,19 @@ if PLOT_BIAS:
 #Set up the domain limits, colorbar limits, all that stuff, for the
 #individual transects 
 #==========================================================================#
-transect_name_list = ['SR1','SR3','P16','A16','I08-09']
-transect_lons_list = [-65,140,-150,-25,95]
-transect_lat_limits_list = [[-65,-55],[-70,-42],[-80,80],[-80,80],[-80,20]]
-MIN_TEMPERATURE          = [-2,-2,-5,-5,-5]
-MAX_TEMPERATURE          = [7 ,12,30,30,30]
+transect_name_list = ['SR3','P16','A16','I08-09']                 ##'SR1',
+transect_lons_list = [140,-150,-25,95]                            ##-65,
+transect_lat_limits_list = [[-67,-42],[-76,61],[-75.01,70],[-65,15.1]] ##[-65,-55],
+MIN_TEMPERATURE          = [-2,-2,-2,-2]                          ##-2,
+MAX_TEMPERATURE          = [13,28,28,28]                          ##7 ,
 
 
 #==========================================================================#
 #Set labels and the cycle lengths for the individual experiments 
 #==========================================================================#
 
-labels  = ['(a) ACCESS-OM2',r'(b) ACCESS-OM2-025','(c) ACCESS-OM2-01', '(d) Observations']
-IAF_CYCLE_LENGTH         = [12,34,24]  ## FIXME - doesn't do all of the period for 01 case
+labels  = ['(a) ACCESS-OM2','(b) ACCESS-OM2','(c) ACCESS-OM2-025','(d) ACCESS-OM2-025','(e) ACCESS-OM2-01', '(f) ACCESS-OM2-01','(g) WOA13','(h) WOA13']
+IAF_CYCLE_LENGTH         = [12,34,24]  ## FIXME - 3rd entry should be 150!!
 
 output_figure_path = './'
 
@@ -60,13 +58,10 @@ MAX_DEPTH = 4500
 
 experiments = ['1deg_jra55v13_iaf_spinup1_B1','025deg_jra55v13_iaf_gmredi6','01deg_jra55v13_iaf']
 #experiments = [ '1deg_jra55v13_iaf_spinup1_A','1deg_jra55v13_iaf_spinup1_A','1deg_jra55v13_iaf_spinup1_A']
-
-
+Nexp = len(experiments)
 
 observations_path     = '/home/157/amh157/v45/amh157/COSIMA_Paper'
 observation_file_name = 'WOA13_temperature_salinity_1985_2013_remap.nc'
-
-
 
 temperature_transect = []
 salt_transect        = [] 
@@ -74,7 +69,7 @@ salt_transect        = []
 MAX_LON_MODEL =   79.5
 MIN_LON_MODEL = -279.5
 
-xticks = np.arange(-80,80.1,10)
+xticks = np.arange(-75,75.1,15)
 xtick_labels = []
 for i_tick in range(xticks.size):
     if xticks[i_tick]<0:
@@ -112,8 +107,10 @@ for i_transect in range(0,len(transect_name_list)):
     #===============================================#
 
     fig = plt.figure(1,figsize=(12,12))
+    plt.subplots_adjust(bottom=0.14,hspace=0.24)
+    
     counter  = 1
-    for i_exp in range(0,len(experiments)):
+    for i_exp in range(0,Nexp):
 
         print('Experiment : ', experiments[i_exp])
         start_index = -IAF_CYCLE_LENGTH[i_exp]
@@ -127,7 +124,7 @@ for i_transect in range(0,len(transect_name_list)):
             temperature = temperature - temperature_obs_on_model_grid
             color_bar_limits = [MIN_TEMP_BIAS,MAX_TEMP_BIAS]
             colormap         = cmocean.cm.balance
-            cbar_label       = r'Temperature Bias ($^{\circ}$C)'
+            cbar_label       = r'Temp Bias ($^{\circ}$C)'
         else:
             color_bar_limits = [MIN_TEMPERATURE[i_transect],MAX_TEMPERATURE[i_transect]]
             colormap         = cmocean.cm.thermal
@@ -141,70 +138,56 @@ for i_transect in range(0,len(transect_name_list)):
         ax.set_xticks(xticks)
         ax.set_xticklabels(xtick_labels)
         ax.set_xlim(transect_lat_limits_list[i_transect])
-        if counter == 3:
-            ax.set_xlabel("Latitude",fontsize=15)
  
         ax.set_yticks(np.arange(-MAX_DEPTH,0.1,1000))
         ax.set_yticklabels(np.arange(MAX_DEPTH,0.1,-1000).astype(np.int))
         ax.set_ylim([-MAX_DEPTH,0])
-        if counter ==1 or counter==3:
-            ax.set_ylabel(r"Depth (m)",fontsize=15) 
+        ax.set_ylabel(r"Depth (m)") 
         #=================================# 
         #Colorbar
         #=================================# 
         if counter == 2:
-            n_colorbar_ticks = 10
-            cbar = fig.colorbar(cs_temp,ax=ax,ticks=np.linspace(color_bar_limits[0],color_bar_limits[1],n_colorbar_ticks))
-            cbar.set_label(cbar_label,fontsize=20)
+            ax1 = plt.axes([0.1,0.08,0.19,0.01])
+            n_colorbar_ticks = 5
+            cbar = fig.colorbar(cs_temp,cax=ax1,orientation='horizontal',ticks=np.linspace(color_bar_limits[0],color_bar_limits[1],n_colorbar_ticks))
+            cbar.set_label(cbar_label)
 
         #=================================#
         #Annotation
         #=================================#
-        ax.annotate(labels[i_exp],
-            xy=(0.5, 1.05), xycoords='axes fraction',
-            horizontalalignment='center', verticalalignment='bottom',fontsize=15,color='black')
+        ax.set_title(labels[2*i_exp])
+        
         counter = counter + 1
 
 
-    ax = fig.add_subplot(4,2,2*counter)
+    ax = fig.add_subplot(427)
     cs_temp = ax.contourf(temperature_transect_obs['yt_ocean'],-temperature_transect_obs['st_ocean'],temperature_transect_obs,np.linspace(MIN_TEMPERATURE[i_transect],MAX_TEMPERATURE[i_transect],20),cmap=cmocean.cm.thermal,extend='both')
-    n_colorbar_ticks = 10
-    cbar = fig.colorbar(cs_temp,ax=ax,ticks=np.linspace(MIN_TEMPERATURE[i_transect],MAX_TEMPERATURE[i_transect],n_colorbar_ticks))
-    cbar.set_label('Temperature ($^{\circ}C$)',fontsize=20)
+    ax2 = plt.axes([0.3,0.08,0.19,0.01])
+    n_colorbar_ticks = 7
+    cbar = fig.colorbar(cs_temp,cax=ax2,orientation='horizontal',ticks=np.linspace(MIN_TEMPERATURE[i_transect],MAX_TEMPERATURE[i_transect],n_colorbar_ticks))
+    cbar.set_label('Temperature ($^{\circ}C$)')
 
     ax.set_xticks(xticks)
     ax.set_xticklabels(xtick_labels)
     ax.set_xlim(transect_lat_limits_list[i_transect])
-    ax.set_xlabel("Latitude",fontsize=15)
+    ax.set_xlabel("Latitude")
 
     ax.set_yticks(np.arange(-MAX_DEPTH,0.1,1000))
     ax.set_yticklabels(np.arange(MAX_DEPTH,0.1,-1000).astype(np.int))
     ax.set_ylim([-MAX_DEPTH,0])
+    ax.set_ylabel(r"Depth (m)")
 
     #=================================# 
     # Annotations
     #=================================# 
-    ax.annotate(labels[3],
-            xy=(0.5, 1.05), xycoords='axes fraction',
-            horizontalalignment='center', verticalalignment='bottom',fontsize=15,color='black')
-
-    output_figure_path = './'
-    figure_file_name   = 'Model_vs_WOA13_Temperature_' + transect_name
-
-    if PLOT_BIAS:
-        figure_file_name = figure_file_name + '_bias'
-    plt.savefig(output_figure_path + figure_file_name + '.pdf', dpi=150)
-    plt.savefig(output_figure_path + figure_file_name + '.png', dpi=150)
-#    plt.savefig(output_figure_path + figure_file_name + '.eps', dpi=150)
-    plt.close('all')
+    ax.set_title(labels[6])
 
     #===============================================#
     # Plot Salinity Sections
     #===============================================#
 
-    fig = plt.figure(2,figsize=(20,10))
     counter  = 1
-    for i_exp in range(0,len(experiments)):
+    for i_exp in range(0,Nexp):
 
         print('Experiment : ', experiments[i_exp])
         start_index = -IAF_CYCLE_LENGTH[i_exp]
@@ -218,13 +201,13 @@ for i_transect in range(0,len(transect_name_list)):
             salt                          = salt - salt_obs_on_model_grid
             color_bar_limits = [MIN_SALT_BIAS,MAX_SALT_BIAS]
             colormap         = cmocean.cm.balance
-            cbar_label       = r'Salinity Bias (psu)'
+            cbar_label       = r'Salt Bias (g/kg)'
         else:
             color_bar_limits = [MIN_SALT,MAX_SALT]
             colormap         = cmocean.cm.haline
-            cbar_label       = r'Salinity (psu)'
+            cbar_label       = r'Salinity (g/kg)'
 
-        ax = fig.add_subplot(2,2,counter)
+        ax = fig.add_subplot(4,2,2*counter)
         cs_salt = ax.contourf(salt['yt_ocean'],-salt['st_ocean'],salt,np.linspace(color_bar_limits[0],color_bar_limits[1],20),cmap=colormap,extend='both')
 
         #=================================# 
@@ -233,34 +216,25 @@ for i_transect in range(0,len(transect_name_list)):
         ax.set_xticks(xticks)
         ax.set_xticklabels(xtick_labels)
         ax.set_xlim(transect_lat_limits_list[i_transect])
-        if counter == 3:
-            ax.set_xlabel("Latitude",fontsize=15)
  
         ax.set_yticks(np.arange(-MAX_DEPTH,0.1,1000))
         ax.set_yticklabels(np.arange(MAX_DEPTH,0.1,-1000).astype(np.int))
-        ax.set_ylim([-MAX_DEPTH,0])
-        if counter ==1 or counter==3:
-            ax.set_ylabel(r"Depth (m)",fontsize=15) 
+        ax.set_ylim([-MAX_DEPTH,0]) 
+        
+        ax.set_title(labels[2*i_exp+1])
         
         #=================================# 
         # Colorbar
         #=================================# 
         if counter == 2:
-            n_colorbar_ticks = 10
-            cbar = fig.colorbar(cs_salt,ax=ax,ticks=np.linspace(color_bar_limits[0],color_bar_limits[1],n_colorbar_ticks))
-            cbar.set_label(cbar_label,fontsize=20)
-
-        #=================================# 
-        # Annotations
-        #=================================# 
-        ax.annotate(labels[i_exp],
-            xy=(0.5, 1.05), xycoords='axes fraction',
-            horizontalalignment='center', verticalalignment='bottom',fontsize=15,color='black')
-
+            ax3 = plt.axes([0.53,0.08,0.19,0.01])
+            n_colorbar_ticks = 5
+            cbar = fig.colorbar(cs_salt,cax=ax3,orientation='horizontal',ticks=np.linspace(color_bar_limits[0],color_bar_limits[1],n_colorbar_ticks))
+            cbar.set_label(cbar_label)
 
         counter = counter + 1
         
-    ax = fig.add_subplot(2,2,counter)
+    ax = fig.add_subplot(428)
     cs_salt = ax.contourf(salt_transect_obs['yt_ocean'],-salt_transect_obs['st_ocean'],salt_transect_obs,np.linspace(MIN_SALT,MAX_SALT,20),cmap=cmocean.cm.haline,extend='both')
     
 
@@ -268,25 +242,24 @@ for i_transect in range(0,len(transect_name_list)):
     ax.set_xticklabels(xtick_labels)
     ax.set_xlim(transect_lat_limits_list[i_transect])
     ax.set_ylim([-MAX_DEPTH,0])
-    ax.set_xlabel("Latitude",fontsize=15)
+    ax.set_xlabel("Latitude")
 
-    n_colorbar_ticks = 10
-    cbar = fig.colorbar(cs_salt,ax=ax,ticks=np.linspace(MIN_SALT,MAX_SALT,n_colorbar_ticks))
-    cbar.set_label('Salinity (psu)',fontsize=20)
+    ax4 = plt.axes([0.74,0.08,0.19,0.01])
+    n_colorbar_ticks = 7
+    cbar = fig.colorbar(cs_salt,cax=ax4,orientation='horizontal',ticks=np.linspace(MIN_SALT,MAX_SALT,n_colorbar_ticks))
+    cbar.set_label('Salinity (g/kg)')
 
-    ax.annotate(labels[3],
-            xy=(0, 1.05), xycoords='axes fraction',
-            horizontalalignment='center', verticalalignment='bottom',fontsize=15,color='black')
+    ax.set_title(labels[7])
 
 
     output_figure_path = './'
-    figure_file_name   = 'Model_vs_WOA13_Salinity_' + transect_name
+    figure_file_name   = 'Model_vs_WOA13_Temp_Salt_' + transect_name
     if PLOT_BIAS:
         figure_file_name = figure_file_name + '_bias'
 
-    plt.savefig(output_figure_path + figure_file_name + '.pdf', dpi=150)
+    #plt.savefig(output_figure_path + figure_file_name + '.pdf', dpi=150)
     plt.savefig(output_figure_path + figure_file_name + '.png', dpi=150)
-#    plt.savefig(output_figure_path + figure_file_name + '.eps', dpi=150)
+    
     plt.close('all')
 
 #    plt.show()     
